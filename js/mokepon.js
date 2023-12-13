795dcc066a630849c54c73e5d29313b5a3910708
EdwardQuevedoP <edwardquevedop@gmail.com>
2023-12-13 18:57:22 -0500

    asta la clase 19

---
+++ js/mokepon.js
@@ -0,0 +1,15 @@
+function seleccionarMascotaJugador() {
+    alert("seleccionaste tu mascota")
+}
+
+var botonMascotaJugador = document.getElementById("boton-mascota")
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+if (){
+
+}else if(){
+
+}else if(){
+    
+}
+
+
