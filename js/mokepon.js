1c0e83a2807550d2dadc482bb666e8bb3cf07a85
EdwardQuevedoP <edwardquevedop@gmail.com>
2023-12-14 18:29:24 -0500

    asta clase 23 primer error

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,67 +1,98 @@
+let ataquejugador = ""
+
+    function iniciarjuego() {
+        let botonMascotaJugador = document.getElementById('boton-mascota')
+        botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+        let botonFuego = document.getElementById('boton-fuego')
+        botonFuego.addEventListener('click', ataqueFuego)
+
+        let botonAgua = document.getElementById('boton-agua')
+        botonAgua.addEventListener('click', ataqueAgua)
+
+        let botonTierra = document.getElementById('boton-tierra')
+        botonTierra.addEventListener('click', ataqueTierra)
+    }
 
 function seleccionarMascotaJugador() {
     let inputHipoge = document.getElementById('Hipoge')
-    let inputCapipepo = document.getElementById('Capipepo') 
-    let inputRatigueya = document.getElementById('Ratigueya') 
+    let inputCapipepo = document.getElementById('Capipepo')
+    let inputRatigueya = document.getElementById('Ratigueya')
     let inputLangostelvis = document.getElementById('Langostelvis')
     let inputTucapalma = document.getElementById('Tucapalma')
     let inputPydos = document.getElementById('Pydos')
 
     let spanMascotaJugador = document.getElementById('mascota-jugador')
-    
-    
-    
+
+
+
     if (inputHipoge.checked) {
         alert("selecionaste a Hipoge")
         spanMascotaJugador.innerHTML = '2'
-    
+
     } else if (inputCapipepo.checked) {
         alert("seleccionaste a Capipepo")
-    
-    }else if (inputRatigueya.checked) {
+
+    } else if (inputRatigueya.checked) {
         alert("seleccionaste a Ratigueya")
-    
-    }else if (inputLangostelvis.checked) {
+
+    } else if (inputLangostelvis.checked) {
         alert("seleccionaste a Langostelvis")
-    
-    }else if (inputTucapalma.checked) {
+
+    } else if (inputTucapalma.checked) {
         alert("seleccionaste a Tucapalma")
-    
-    }else if (inputPydos.checked) {
+
+    } else if (inputPydos.checked) {
         alert("seleccionaste a Pydos")
-    
+
     }
     seleccionarMascotaEnemigo()
 }
 
-function seleccionarMascotaEnemigo(){
-    let ataqueAleatorio = Aleatorio(1,6)
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = Aleatorio(1, 6)
     let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
 
-    if(ataqueAleatorio == 1){
-       spanMascotaEnemigo.innerHTML='Hipoge'
-       //Hipoge
-    }else if (ataqueAleatorio == 2){
-        spanMascotaEnemigo.innerHTML='capipepo'
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipoge'
+        //Hipoge
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'capipepo'
         //capipepo
-    }else if(ataqueAleatorio == 3){
-        spanMascotaEnemigo.innerHTML='ratigueya'
+    } else if (ataqueAleatorio == 3) {
+        spanMascotaEnemigo.innerHTML = 'ratigueya'
         //ratigueya
-    }else if(ataqueAleatorio == 4){
-        spanMascotaEnemigo.innerHTML='Langostelvis'
+    } else if (ataqueAleatorio == 4) {
+        spanMascotaEnemigo.innerHTML = 'Langostelvis'
         //Langostelvis
-    }else if(ataqueAleatorio == 5){
-        spanMascotaEnemigo.innerHTML='Tucapalma'
+    } else if (ataqueAleatorio == 5) {
+        spanMascotaEnemigo.innerHTML = 'Tucapalma'
         //Tucapalma
-    }else if(ataqueAleatorio == 6){
-        spanMascotaEnemigo.innerHTML='Pydos'
+    } else if (ataqueAleatorio == 6) {
+        spanMascotaEnemigo.innerHTML = 'Pydos'
         //Pydos
     }
 
-} 
+}
 
-function Aleatorio(min,max){
-    return Math.floor(Math.random() * (max - min +1)+min)
+function ataqueFuego() {
+    ataquejugador = 'FUEGO'
+    alert(ataquejugador)
+}
+
+
+function ataqueAgua() {
+    ataquejugador = 'AGUA'
+    alert(ataquejugador)
+}
+
+
+function ataqueTierra() {
+    ataquejugador = 'TIERRA'
+    alert(ataquejugador)
+}
+
+function Aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 
